80b408ff91529a1812dd1be8b98726879e4517aa
Idumi888 <vincent.dievart@epsi.fr>
2021-06-21 14:34:49 +0000

    calibre fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Calibre;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    
+    public function __construct(){
+        $this->faker=Factory::create("fr_FR");
+        
+    }
+
+
+        
+    
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+      
+        
+        
+        
+        $calibres = [
+            1 => [
+                'calibre' => '35/55'
+            ],
+            2 => [
+                'calibre' => '40/75'
+            ],
+            3 => [
+                'calibre' => '50/75'
+            ]
+            ];
+
+            foreach($calibres as $key => $value){
+                $calibre = new Calibre();
+                $calibre->setCalibre($value['calibre']);
+                $manager->persist($calibre);
+
+                
+                
+            }
+
+            $manager->flush();
+        
+    }
+
+}

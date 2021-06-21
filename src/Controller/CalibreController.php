a4ea2d74e76c0bd1495b529eb793b1a5f8b735c8
Idumi888 <vincent.dievart@epsi.fr>
2021-06-21 14:48:18 +0000

    calibre

--- src/Controller/CalibreController.php
+++ src/Controller/CalibreController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\CalibreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Calibre;
 
 class CalibreController extends AbstractController
 {
     /**
      * @Route("/calibre", name="calibre")
      */
-    public function index(): Response
+    public function theme(CalibreRepository $calibreRepository, Request $request): Response
     {
-        return $this->render('calibre/index.html.twig', [
-            'controller_name' => 'CalibreController',
+        $em = $this->getDoctrine();
+        $repoCalibre = $em->getRepository(Calibre::class);
+        
+        $calibres = $repoCalibre->findBy(array(), array('calibre' => 'ASC'));
+        return $this->render('calibre/calibres.html.twig', [
+            'calibres' => $calibres
+           
         ]);
     }
 }
